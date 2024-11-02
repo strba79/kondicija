7e5a6886d671d0aa8e440c42a424240911be5386
Nenad Strbic <nenad.strbic@gmail.com>
2024-11-02 14:27:47 +0100

    add prepare fragment with counter

--- app/src/main/java/com/strba/kondicija/MainPresenter.kt
+++ app/src/main/java/com/strba/kondicija/MainPresenter.kt
@@ -1,11 +1,12 @@
 import com.strba.kondicija.Contract
 import com.strba.kondicija.InputFragment
 import com.strba.kondicija.MainActivity
+import com.strba.kondicija.PrepareFragment
 import com.strba.kondicija.TimerService
 
 class MainPresenter(private val view: MainActivity) : Contract.Presenter, TimerService.TimerListener {
     private var timerService: TimerService? = null
-
+    private var prepareFragment: PrepareFragment? = null
     override fun startTraining(sets: Int, workMinutes: Int, workSeconds: Int, restSeconds: Int) {
         timerService?.startTraining(sets, workMinutes, workSeconds, restSeconds)
     }
@@ -28,10 +29,15 @@ class MainPresenter(private val view: MainActivity) : Contract.Presenter, TimerS
     }
 
     override fun onPrepareStart() {
+        prepareFragment = PrepareFragment()
         view.showPrepareFragment()
     }
 
     override fun onTimerUpdate(time: String, setsRemaining: Int, isWork: Boolean) {
+        if (!isWork) {
+            val secondsRemaining = Regex("\\d+").find(time)?.value?.toLong() ?: 0L
+            prepareFragment?.updateSeconds(secondsRemaining)
+        }
         view.updateTimer(time, setsRemaining, isWork)
     }
 
